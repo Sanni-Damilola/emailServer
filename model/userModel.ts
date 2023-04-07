0027cffbd46c8f41d3f698b2e3fa99b286739205
Damilola Sanni <sannifortune11@gmail.com>
2023-04-07 13:10:11 +0100

    done

--- model/userModel.ts
+++ model/userModel.ts
@@ -1,37 +1,31 @@
 import mongoose from "mongoose";
 
-
-
 interface Iuser {
-    name: string;
-    email: string;
-    password: string;
-    token: string;
-    verified: boolean;
+  name: string;
+  email: string;
+  password: string;
+  token: string;
+  verified: boolean;
 }
 
-interface userData extends Iuser, mongoose.Document{ }
-
-
+interface userData extends Iuser, mongoose.Document {}
 
 const userModel = new mongoose.Schema({
-    name: {
-        type: String,
-    },
-    email: {
-        type: String,
-    },
-    password: {
-        type: String,
-    },
-    token: {
-        type: String,
-    },
-    verified: {
-        type: Boolean,
-    },
-   
-   
-})
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  token: {
+    type: String,
+  },
+  verified: {
+    type: Boolean,
+  },
+});
 
-
+export default mongoose.model<userData>("emailServer", userModel);
