b0834538a088ebacc569a27c8dd5e55a8d35f286
Damilola Sanni <sannifortune11@gmail.com>
2023-04-07 14:09:08 +0100

    update

--- controller/controller.ts
+++ controller/controller.ts
@@ -1,3 +1,10 @@
+import { Request, Response } from "express";
 import userModel from "../model/userModel";
 
-
+export const getCreate = async (req: Request, res: Response) => {
+  try {
+    const { name, email, password, token, verified, otp } = req.body;
+  } catch (error) {
+    console.log("an Error occured in getCreate", error);
+  }
+};
--- model/userModel.ts
+++ model/userModel.ts
@@ -5,6 +5,7 @@ interface Iuser {
   email: string;
   password: string;
   token: string;
+  otp: string;
   verified: boolean;
 }
 
